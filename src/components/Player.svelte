42fe16449bf89a5e5c447021537f529b2c1ab129
Joe Astuccio <joe@astucc.io>
2021-11-26 07:11:05 -0500

    Add media player with playback speed controls

---
+++ src/components/Player.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let url;
+  let playbackRate = 1.0;
+</script>
+
+<audio bind:playbackRate src={url} controls />
+<select bind:value={playbackRate} name="playback" id="playback">
+  <option value={1.0}>1</option>
+  <option value={1.5}>1.5</option>
+  <option value={2.0}>2</option>
+</select>
